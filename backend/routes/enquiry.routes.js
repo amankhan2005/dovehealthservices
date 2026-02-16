493835a6f86af5d522294954c3110a68e153f7bd
Aman Khan <amankhan099k@gmail.com>
2026-02-16 18:00:03 +0530

    enquirt routes fixed

--- backend/routes/enquiry.routes.js
+++ backend/routes/enquiry.routes.js
@@ -1,18 +1,15 @@
-import express from "express";
+ import express from "express";
 
 import {
   createEnquiry
 }
 from "../controllers/enquiry.Controllers.js";
 
-
 const router = express.Router();
 
-
 router.post(
-  "/enquiry",
+  "/",    
   createEnquiry
 );
-
 
 export default router;
